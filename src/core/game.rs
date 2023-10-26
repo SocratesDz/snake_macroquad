26b97cc11f1a930eee967a9fe9b7f26fcabd0c2b
SocratesDz <sdsxd1800@gmail.com>
2023-10-26 17:13:20 -0400

    feat: check for collisions. add score. must fix tests.

--- src/core/game.rs
+++ src/core/game.rs
@@ -1,10 +1,13 @@
 use super::{board::Board, cell::CellType, snake::Snake};
 
+pub const FOOD_BASE_VALUE: u32 = 1;
+
 pub struct Game {
     // TODO: Add current state
     // TOD: Add queue of events
     pub board: Board,
     pub snake: Snake,
+    pub score: u32,
 }
 
 impl Game {
@@ -12,27 +15,38 @@ impl Game {
         Self {
             board: Board::new(rows, columns),
             snake: Snake::new((rows / 2) as i32, (columns / 2) as i32),
+            score: 0,
         }
     }
 
     pub fn update(&mut self) {
         self.snake.update();
         // Check for snake collision
-        if self
+        if let Some((index, cell)) = self
             .snake
             .body
             .iter()
-            .chain(
-                self.board
-                    .cells
-                    .iter()
-                    .filter(|c| c.cell_type == CellType::Wall),
-            )
-            .any(|c| c.position() == self.snake.head.position())
+            .chain(self.board.cells.iter())
+            .enumerate()
+            .find(|(_, cell)| cell.position() == self.snake.head.position())
         {
-            // Snake collided
+            match cell.cell_type {
+                CellType::SnakeBody | CellType::Wall => {
+                    // Emit hit event
+                    // Then emit game over state
+                }
+                CellType::Food => {
+                    self.snake.grow();
+                    
+                    self.board.cells.remove(index);
+
+                    self.generate_food();
+
+                    self.score += FOOD_BASE_VALUE;
+                }
+                _ => {}
+            }
         }
-        // Check for snake points
     }
 
     fn generate_food(&mut self) {
